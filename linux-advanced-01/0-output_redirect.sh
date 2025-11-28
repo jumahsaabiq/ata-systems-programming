9d65a5c15f6f6a5feb15e24fe7fce5211a1bb96d
jumahsaabiq <jumahmobolaji@gmail.com>
2025-11-28 18:37:53 +0100

    Add first text

--- linux-advanced-01/0-output_redirect.sh
+++ linux-advanced-01/0-output_redirect.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Print to stdout
 echo "Hello, World!"
 
-# Overwrite the file with first line
 echo "First line" > 0-first_output.txt
-
-# Append second line
 echo "Second line" >> 0-first_output.txt
 
-# Display the file content
 cat 0-first_output.txt
